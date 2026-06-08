d89cf967249ac33836fa12bde273446fa1393f0b
N-Tien-L <tienlamnhatt@gmail.com>
2026-06-09 00:36:17 +0200

    test: align market controller tests with security

--- services/market-service/src/test/java/com/stockwise/market/adapter/in/web/MarketControllerTest.java
+++ services/market-service/src/test/java/com/stockwise/market/adapter/in/web/MarketControllerTest.java
@@ -10,6 +10,7 @@ import com.stockwise.market.security.JwtTokenProvider;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = MarketController.class)
+@AutoConfigureMockMvc(addFilters = false)
 @Import(com.stockwise.market.exception.GlobalExceptionHandler.class)
 class MarketControllerTest {
 
